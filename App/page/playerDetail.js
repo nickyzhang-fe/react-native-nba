847aa675220317fd75bc4b0aa96f392aadd9c3c2
ibinbin <zhangxia2013105@163.com>
2017-12-20 16:26:15 +0800

    添加球员信息

--- App/page/playerDetail.js
+++ App/page/playerDetail.js
@@ -32,63 +32,134 @@ class PlayerDetail extends Component {
             playerIcon: this.props.playerInfo.playerIcon,
             teamIcon: this.props.playerInfo.teamIcon,
             seasonData: {},
-            baseInfo: {}
+            baseInfo: {},
+            regularSeason: [],
+            postSeason: [],
+            header: [],
+            stats: [],
+            lastMatches: []
         };
-        console.log(this.props.playerInfo)
     };
 
     componentDidMount() {
-        InteractionManager.runAfterInteractions(() => this.getPlayerBaseInfo())
+        InteractionManager.runAfterInteractions(() => this.getPlayerBaseInfo());
+        InteractionManager.runAfterInteractions(() => this.getPlayerCareerData());
+        InteractionManager.runAfterInteractions(() => this.getPlayerSeasonData());
     }
 
     render() {
-        const {baseInfo, seasonData} = this.state;
+        const {baseInfo, seasonData, regularSeason, header, postSeason, lastMatches, stats} = this.state;
         return (
             <View style={styles.container}>
                 <HeaderBar
                     title={this.state.title}
                     showLeftState={true}
                     showRightState={true}
-                    showRightImage={true}
+                    showRightImage={false}
                     leftItemTitle={'资料'}
                     leftImageSource={require('../image/back.png')}
-                    rightImageSource={{uri: this.state.teamIcon}}
                     onPress={() => this.goBack()}/>
                 <View style={styles.outline}>
                     <Image style={styles.teamLogo} source={{uri: this.state.playerIcon}}/>
                     <View style={{alignItems: 'flex-end'}}>
                         <Text style={[styles.outlineTxt, {fontSize: 30}]}>{`#${baseInfo.jerseyNum}`}</Text>
-                        <Text style={[styles.outlineTxt, styles.outlineTxtSize]}>{`${baseInfo.teamName}  ${baseInfo.position}  ${baseInfo.height}  ${baseInfo.weight}`}</Text>
-                        <Text style={[styles.outlineTxt, styles.outlineTxtSize]}>{`生日:${baseInfo.birthDate}  选秀${baseInfo.draftYear}`}</Text>
+                        <Text
+                            style={[styles.outlineTxt, styles.outlineTxtSize]}>{`${baseInfo.teamName}  ${baseInfo.position}  ${baseInfo.height}  ${baseInfo.weight}`}</Text>
+                        <Text
+                            style={[styles.outlineTxt, styles.outlineTxtSize]}>{`生日:${baseInfo.birthDate}  选秀${baseInfo.draftYear}`}</Text>
                     </View>
                 </View>
-                <View style={styles.seasonData}>
-                    <View style={styles.dataItem}>
-                        <Text style={styles.dataValue}>{seasonData.points}</Text>
-                        <Text style={styles.dataKey}>{`场均得分`}</Text>
+                <ScrollView>
+                    <View style={styles.seasonData}>
+                        <View style={styles.dataItem}>
+                            <Text style={styles.dataValue}>{seasonData.points}</Text>
+                            <Text style={styles.dataKey}>{`场均得分`}</Text>
+                        </View>
+                        <View style={styles.dataItem}>
+                            <Text style={styles.dataValue}>{seasonData.rebounds}</Text>
+                            <Text style={styles.dataKey}>{`场均篮板`}</Text>
+                        </View>
+                        <View style={styles.dataItem}>
+                            <Text style={styles.dataValue}>{seasonData.assists}</Text>
+                            <Text style={styles.dataKey}>{`场均助攻`}</Text>
+                        </View>
+                        <View style={styles.dataItem}>
+                            <Text style={styles.dataValue}>{seasonData.blocks}</Text>
+                            <Text style={styles.dataKey}>{`场均盖帽`}</Text>
+                        </View>
+                        <View style={styles.dataItem}>
+                            <Text style={styles.dataValue}>{seasonData.steals}</Text>
+                            <Text style={styles.dataKey}>{`场均抢断`}</Text>
+                        </View>
                     </View>
-                    <View style={styles.dataItem}>
-                        <Text style={styles.dataValue}>{seasonData.rebounds}</Text>
-                        <Text style={styles.dataKey}>{`场均篮板`}</Text>
+                    <View style={styles.subTitle}>
+                        <Text style={styles.subTitleTxt}>{'常规赛平均'}</Text>
                     </View>
-                    <View style={styles.dataItem}>
-                        <Text style={styles.dataValue}>{seasonData.assists}</Text>
-                        <Text style={styles.dataKey}>{`场均助攻`}</Text>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View>
+                                {
+                                    regularSeason.map((item, index) => this.renderRegularItem(item, index))
+                                }
+                            </View>
+                        </ScrollView>
                     </View>
-                    <View style={styles.dataItem}>
-                        <Text style={styles.dataValue}>{seasonData.blocks}</Text>
-                        <Text style={styles.dataKey}>{`场均盖帽`}</Text>
+                    <View style={styles.subTitle}>
+                        <Text style={styles.subTitleTxt}>{'季后赛平均'}</Text>
                     </View>
-                    <View style={styles.dataItem}>
-                        <Text style={styles.dataValue}>{seasonData.steals}</Text>
-                        <Text style={styles.dataKey}>{`场均抢断`}</Text>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View>
+                                {
+                                    postSeason.map((item, index) => this.renderRegularItem(item, index))
+                                }
+                            </View>
+                        </ScrollView>
                     </View>
-                </View>
+                    <View style={styles.subTitle}>
+                        <Text style={styles.subTitleTxt}>{'本赛季平均'}</Text>
+                    </View>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View>
+                                {
+                                    stats.map((item, index) => this.renderRegularItem(item, index))
+                                }
+                            </View>
+                        </ScrollView>
+                    </View>
+                    <View style={styles.subTitle}>
+                        <Text style={styles.subTitleTxt}>{'最近5场'}</Text>
+                    </View>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View>
+                                {
+                                    lastMatches.map((item, index) => this.renderRegularItem(item, index, '4'))
+                                }
+                            </View>
+                        </ScrollView>
+                    </View>
+                </ScrollView>
             </View>
         )
     }
 
+    renderRegularItem = (items, indexs, data) => {
+        return (
+            <View key={indexs} style={styles.scrollItem}>
+                {
+                    items.map((item, index) => this.renderItem(item, index, data))
+                }
+            </View>
+        )
+    };
 
+    renderItem = (item, index, data) => {
+        return (
+            <Text key={index} style={[styles.scrollItemTxt, {width: (index === 0 && data === '4') ? 120 : 66}]}>{item}</Text>
+        )
+    };
 
 
     goBack = () => {
@@ -101,7 +172,6 @@ class PlayerDetail extends Component {
         =09385DB300E081E142ED046B568B2E48&from=app&guid=09385DB300E081E142ED046B568B2E48&height
         =1920&network=WIFI&os=Android&osvid=7.1.1&width=1080&playerId=${this.state.playerId}`;
         NetUtil.get(url, function (res) {
-            console.log(res.data);
             that.setState({
                 baseInfo: res.data,
                 seasonData: res.data.stats
@@ -109,6 +179,35 @@ class PlayerDetail extends Component {
         })
     };
 
+    getPlayerCareerData = () => {
+        let that = this;
+        let url = `http://sportsnba.qq.com/player/stats?appver=4.0.1&appvid=4.0.1&deviceId
+        =09385DB300E081E142ED046B568B2E48&from=app&guid=09385DB300E081E142ED046B568B2E48&height
+        =1920&network=WIFI&os=Android&osvid=7.1.1&width=1080&playerId=${this.state.playerId}&tabType=2`;
+        NetUtil.get(url, function (res) {
+            let tempRegular = res.data.reg.rows.unshift(res.data.reg.head);
+            let tempPost = res.data.playoff.rows.unshift(res.data.reg.head);
+            that.setState({
+                regularSeason: res.data.reg.rows,
+                postSeason: res.data.playoff.rows,
+            })
+        })
+    };
+
+    getPlayerSeasonData = () => {
+        let that = this;
+        let url = `http://sportsnba.qq.com/player/stats?appver=4.0.1&appvid=4.0.1&deviceId
+        =09385DB300E081E142ED046B568B2E48&from=app&guid=09385DB300E081E142ED046B568B2E48&height
+        =1920&network=WIFI&os=Android&osvid=7.1.1&width=1080&playerId=${this.state.playerId}&tabType=1`;
+        NetUtil.get(url, function (res) {
+            let tempLastMatches = res.data.lastMatches.rows.unshift(res.data.lastMatches.head);
+            let tempStats = res.data.stats.rows.unshift(res.data.stats.head);
+            that.setState({
+                lastMatches: res.data.lastMatches.rows,
+                stats: res.data.stats.rows,
+            })
+        })
+    }
 }
 
 
@@ -139,11 +238,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         height: 70,
         width: CommonUtil.getScreenWidth(),
-        borderBottomWidth: 1,
-        borderBottomColor: CommonStyle.GRAY_COLOR
     },
     dataItem: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -155,6 +252,30 @@ const styles = StyleSheet.create({
         fontSize: 24,
         color: CommonStyle.BLACK,
         marginVertical: 5
+    },
+    subTitle: {
+        height: 40,
+        width: CommonUtil.getScreenWidth(),
+        borderBottomWidth: 1,
+        borderBottomColor: CommonStyle.GRAY_COLOR,
+        borderTopWidth: 1,
+        borderTopColor: CommonStyle.GRAY_COLOR,
+        justifyContent: 'center',
+        paddingLeft: 15
+    },
+    subTitleTxt: {
+        color: CommonStyle.TEXT_GRAY_COLOR,
+    },
+    scrollItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 40,
+        justifyContent:'center'
+    },
+    scrollItemTxt: {
+        width: 66,
+        textAlign: 'center',
+        color: CommonStyle.BLACK
     }
 });
 
